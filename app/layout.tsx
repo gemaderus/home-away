2339fe5627e8d53a59c093488138167157f9736f
gemaderus <hi@gemaderus.com>
2024-09-30 16:18:55 +0200

    feat: add clerk provider and customs styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,12 @@
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import Providers from "./providers";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const geistSans = localFont({
-	src: "./fonts/GeistVF.woff",
-	variable: "--font-geist-sans",
-	weight: "100 900",
-});
-const geistMono = localFont({
-	src: "./fonts/GeistMonoVF.woff",
-	variable: "--font-geist-mono",
-	weight: "100 900",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "HomeAway",
@@ -24,12 +19,17 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				{children}
-			</body>
-		</html>
+		<ClerkProvider
+			publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+		>
+			<html lang="en" suppressHydrationWarning>
+				<body className={inter.className}>
+					<Providers>
+						<Navbar />
+						<main className="container py-10">{children}</main>
+					</Providers>
+				</body>
+			</html>
+		</ClerkProvider>
 	);
 }
