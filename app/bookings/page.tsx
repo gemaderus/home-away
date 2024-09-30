7d3d17b3bf0fe9471dc2e28afafd51bf8417f0da
gemaderus <hi@gemaderus.com>
2024-09-30 12:10:20 +0200

    feat: add basic pages

---
+++ app/bookings/page.tsx
@@ -0,0 +1,5 @@
+const BookingPage = () => {
+	return <h1 className="text-3xl">BookingsPage</h1>;
+};
+
+export default BookingPage;
---
+++ app/profiles/page.tsx
@@ -0,0 +1,5 @@
+const ProfilesPage = () => {
+	return <h1 className="text-3xl">CheckoutPage</h1>;
+};
+
+export default ProfilesPage;
