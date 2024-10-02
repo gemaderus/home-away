2efda371f3fb2dde7e9917388c93f5edf73dce5d
gemaderus <hi@gemaderus.com>
2024-10-02 11:46:04 +0200

    feat: add logic to redirect the user

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -2,8 +2,14 @@ import FormInput from "../../form/FormInput";
 import FormContainer from "@/components/form/FormContainer";
 import { SubmitButton } from "@/app/form/Buttons";
 import { createProfileAction } from "@/utils/actions.ts";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const CreateProfilePage = () => {
+const CreateProfilePage = async () => {
+	const user = await currentUser();
+
+	if (user?.privateMetadata?.hasProfile) redirect("/");
+
 	return (
 		<section>
 			<h1 className="text-2xl font-semibold mb-8 capitalize">New user</h1>
