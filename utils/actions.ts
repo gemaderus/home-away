be8550a7c51b81d2d6000b723f784a4f483798ae
gemaderus <hi@gemaderus.com>
2024-11-07 06:30:41 +0100

    feat: add actions for profile and images

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,10 +1,29 @@
 "use server";
 
-import { profileSchema } from "./schemas";
+import { imageSchema, profileSchema, propertySchema, validateWithZodSchema } from "./schemas";
 import db from "./db";
-import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
+import { clerkClient, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { uploadImage } from "./supabase";
+
+const getAuthUser = async () => {
+	const user = await currentUser();
+
+	if (!user) {
+		throw new Error("You must be login to access this route");
+	}
+
+	if (!user.privateMetadata.hasProfile) redirect("/profile/create");
+
+	return user;
+};
+
+const renderError = (error: unknown): { message: string } => {
+	return {
+		message: error instanceof Error ? error.message : "An error ocurred",
+	};
+};
 
 export const createProfileAction = async (
 	prevState: any,
@@ -55,4 +74,95 @@ export const fetchProfileImage = async () => {
 	});
 
 	return profile?.profileImage;
+};
+
+export const fetchProfile = async () => {
+	const user = await getAuthUser();
+	const profile = await db.profile.findUnique({
+		where: {
+			clerkId: user.id,
+		},
+	});
+
+	if (!profile) redirect("/profile/create");
+	return profile;
+};
+
+export const updateProfileAction = async (
+	prevState: any,
+	formData: FormData
+): Promise<{ message: string }> => {
+	const user = await getAuthUser();
+
+	try {
+		const rawData = Object.fromEntries(formData);
+		const validatedFields = validateWithZodSchema(profileSchema, rawData);
+
+		await db.profile.update({
+			where: {
+				clerkId: user.id,
+			},
+
+			data: validatedFields,
+		});
+
+		revalidatePath("/profile");
+	} catch (error) {
+		console.log("error", error);
+		return renderError(error);
+	}
+	redirect("/");
+};
+
+export const updateProfileImageAction = async (
+	prevState: any,
+	formData: FormData
+): Promise<{ message: string }> => {
+	const user = await getAuthUser();
+
+	try {
+
+		const image = formData.get("image") as File;
+	const validateFields = validateWithZodSchema(imageSchema, { image });
+	const fullPath = await uploadImage(validateFields.image);
+	
+
+	await db.profile.update({
+		where: {
+			clerkId: user.id,
+		},
+
+		data: {
+			profileImage: fullPath,
+		},
+	})
+
+	revalidatePath("/profile");
+	return { message: "Profile image updated successfully" };
+
+	} catch (error) {
+		console.log('error', error)
+		return renderError(error);
+
+	}
+};
+
+export const createPropertyAction = async (
+	prevState: any,
+	formData: FormData
+): Promise<{ message: string }> => {
+	const user = await getAuthUser();
+
+	try {
+
+		const rawData = Object.fromEntries(formData);
+		const validatedFields = validateWithZodSchema(propertySchema, rawData);
+		return { message: "Property created successfully" };
+
+	} catch (error) {
+	
+		return renderError(error);
+	}
+
+	//redirect("/");
 };
