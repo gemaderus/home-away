ec48f6314ef06c9e60108a9a1741a084d833dd9d
gemaderus <hi@gemaderus.com>
2024-11-07 06:29:13 +0100

    feat: add profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,44 @@
-const ProfilePage = () => {
-	return <h1 className="text-3xl">ProfilePage</h1>;
+import FormInput from "@/components/form/FormInput";
+import FormContainer from "@/components/form/FormContainer";
+import { SubmitButton } from "@/components/form/Buttons";
+import { fetchProfile, updateProfileAction, updateProfileImageAction } from "@/utils/actions";
+import ImageInputContainer from "@/components/form/ImageInputContainer";
+
+const ProfilePage = async () => {
+	const profile = await fetchProfile();
+
+	return (
+		<section>
+			<h1 className="text-2xl font-semibold mb-8 capitalize">User profile</h1>
+			<div className="border p-8 rounded-md">
+				<ImageInputContainer image={profile.profileImage} name={profile.username} action={updateProfileImageAction} text="Update Profile Image" />
+				<p>hello</p>
+				<FormContainer action={updateProfileAction}>
+					<div className="grid md:grid-cols-2 gap-4 mt-4">
+						<FormInput
+							name="firstName"
+							type="text"
+							label="First Name"
+							defaultValue={profile.firstName}
+						/>
+						<FormInput
+							name="lastName"
+							type="text"
+							label="Last Name"
+							defaultValue={profile.lastName}
+						/>
+						<FormInput
+							name="username"
+							type="text"
+							label="Username"
+							defaultValue={profile.username}
+						/>
+					</div>
+					<SubmitButton text="Update user" className="mt-8" />
+				</FormContainer>
+			</div>
+		</section>
+	);
 };
 
 export default ProfilePage;
