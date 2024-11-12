59f004ee4ab47bb14e25290455a3756b8946e314
gemaderus <hi@gemaderus.com>
2024-11-12 06:23:31 +0100

    feat: add logic for CategoriesList and Empty view

--- components/home/CategoriesList.tsx
+++ components/home/CategoriesList.tsx
@@ -1,9 +1,37 @@
-type CategoriesListProps = {
+import { categories } from '@/utils/categories';
+import Link from 'next/link';
+import { ScrollArea, ScrollBar } from '../ui/scroll-area';
+
+export type SearchParamsProps = {
   category?: string;
+  search?: string;
 };
 
-const CategoriesList = ({ category }: CategoriesListProps) => {
-  return <div>CategoriesList</div>;
+const CategoriesList = ({ category, search }: SearchParamsProps) => {
+  const searchTerm = search ? `&search=${search}` : '';
+  return (
+    <section>
+      <ScrollArea>
+        <div className="flex gap-x-4">
+          {categories.map((cat) => {
+            const isActive = cat.label === category;
+
+            return (
+              <Link key={cat.label} href={`/?category=${cat.label}${searchTerm}`}>
+                <article
+                  className={`p-3 flex flex-col items-center cursor-pointer duration-300 hover:text-primary w-[100px] ${isActive ? 'text-primary' : ''}`}
+                >
+                  <cat.icon className="w-6 h-6" />
+                  <span>{cat.label}</span>
+                </article>
+              </Link>
+            );
+          })}
+        </div>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
+    </section>
+  );
 };
 
 export default CategoriesList;
--- components/home/EmptyList.tsx
+++ components/home/EmptyList.tsx
@@ -1,5 +1,23 @@
-const EmptyList = () => {
-  return <div>Empty list</div>;
+import { Button } from '../ui/button';
+
+type EmptyListProps = {
+  heading?: string;
+  message?: string;
+  btnText?: string;
+};
+
+const EmptyList = ({
+  heading = 'No items in the list',
+  message = 'Keep exploring our properties',
+  btnText = 'back home',
+}) => {
+  return (
+    <div className="mt-4">
+      <h2 className="text-xl font-bold">{heading}</h2>
+      <p className="text-lg">{message}</p>
+      <Button className="mt-2">{btnText}</Button>
+    </div>
+  );
 };
 
 export default EmptyList;
