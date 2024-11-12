2dcacb0bd3cd2152216c20166532ad5dcbe6d90f
gemaderus <hi@gemaderus.com>
2024-11-12 06:24:21 +0100

    feat: add logic for property container

--- components/home/PropertiesContainer.tsx
+++ components/home/PropertiesContainer.tsx
@@ -1,10 +1,18 @@
-const PropertiesContainer = () => {
-  return (
-    <div>
-      Properties container wjlsdjld saljsallas asldjsal lkjdsa dsakljsad dasd sadlknasd lsdamlsa
-      saldsald saldjsald qqqqqqq
-    </div>
-  );
+import { fetchProperties } from '@/utils/actions';
+import type { PropertyCardProps } from '@/utils/types';
+import { SearchParamsProps } from './CategoriesList';
+import EmptyList from './EmptyList';
+import PropertiesList from './PropertiesList';
+
+const PropertiesContainer = async ({ category, search }: SearchParamsProps) => {
+  const properties: PropertyCardProps[] = await fetchProperties({ search, category });
+
+  if (properties.length === 0) {
+    return (
+      <EmptyList heading="No results" message="Try changing or removing some of our filters" />
+    );
+  }
+  return <PropertiesList properties={properties} />;
 };
 
 export default PropertiesContainer;
