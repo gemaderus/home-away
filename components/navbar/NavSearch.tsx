53b6015b6cd0028a0881b381e99d3d08a04369be
gemaderus <hi@gemaderus.com>
2024-11-12 06:25:19 +0100

    feat: add logic for search

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,7 +1,46 @@
+'use client';
+
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { useDebouncedCallback } from 'use-debounce';
 import { Input } from '../ui/input';
 
 const NavSearch = () => {
-  return <Input type="text" placeholder="find a property..." className="max-w-xs dark:bg-muted" />;
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const { replace } = useRouter();
+
+  const [search, setSearch] = useState(searchParams.get('search')?.toString() || '');
+
+  const handleSearch = useDebouncedCallback((value: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (value) {
+      params.set('search', value);
+    } else {
+      params.delete('search');
+    }
+
+    replace(`${pathName}?${params.toString()}`);
+  });
+
+  useEffect(() => {
+    if (!searchParams.get('search')) {
+      setSearch('');
+    }
+  }, [searchParams.get('search')]);
+
+  return (
+    <Input
+      type="text"
+      placeholder="find a property..."
+      className="max-w-xs dark:bg-muted"
+      value={search}
+      onChange={(e) => {
+        setSearch(e.target.value);
+        handleSearch(e.target.value);
+      }}
+    />
+  );
 };
 
 export default NavSearch;
