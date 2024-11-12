bfe2e860c5aa57e837396e87dd54eba58248efed
gemaderus <hi@gemaderus.com>
2024-11-12 06:24:37 +0100

    feat: add logic for property list

--- components/home/PropertiesList.tsx
+++ components/home/PropertiesList.tsx
@@ -1,5 +1,18 @@
-const PropertiesList = () => {
-  return <div>Properties container</div>;
+import { PropertyCardProps } from '@/utils/types';
+import PropertyCard from '../card/PropertyCard';
+
+type PropertiesListProps = {
+  properties: PropertyCardProps[];
+};
+
+const PropertiesList = ({ properties }: PropertiesListProps) => {
+  return (
+    <section className="mt-4 gap-8 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {properties.map((property) => (
+        <PropertyCard key={property.id} property={property} />
+      ))}
+    </section>
+  );
 };
 
 export default PropertiesList;
