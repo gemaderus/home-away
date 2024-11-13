6a76becf9ec2c7a92a49e7335dfee64a97db0fa1
gemaderus <hi@gemaderus.com>
2024-11-13 10:52:21 +0100

    feat: add Card buttons

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -1,7 +1,9 @@
 'use client';
 import { Button } from '@/components/ui/button';
+import { SignInButton } from '@clerk/nextjs';
 import { ReloadIcon } from '@radix-ui/react-icons';
 import { useFormStatus } from 'react-dom';
+import { FaHeart, FaRegHeart } from 'react-icons/fa';
 
 type btnSize = 'default' | 'lg' | 'sm';
 
@@ -27,6 +29,36 @@ export const SubmitButton = ({
         </>
       ) : (
         text
+      )}
+    </Button>
+  );
+};
+
+export const CardSignInButton = () => {
+  return (
+    <SignInButton mode="modal">
+      <Button type="button" size="icon" variant="outline" className="p-2 cursor-pointer" asChild>
+        <FaRegHeart />
+      </Button>
+    </SignInButton>
+  );
+};
+
+type CardSubmitButtonProps = {
+  isFavorite: boolean;
+};
+
+export const CardSubmitButton = ({ isFavorite }: CardSubmitButtonProps) => {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button type="submit" size="icon" variant="outline" className="p-2 cursor pointer">
+      {pending ? (
+        <ReloadIcon className="animate-spin" />
+      ) : isFavorite ? (
+        <FaHeart />
+      ) : (
+        <FaRegHeart />
       )}
     </Button>
   );
