0cde735eea228a63298b0763d9992cb07272734a
gemaderus <hi@gemaderus.com>
2024-11-13 10:49:49 +0100

    feat: add favorite components

--- components/card/FavoriteToggleButton.tsx
+++ components/card/FavoriteToggleButton.tsx
@@ -1,16 +1,23 @@
+import { CardSignInButton } from '@/components/form/Buttons';
+import { fetchFavoriteId } from '@/utils/actions';
+import { auth } from '@clerk/nextjs/server';
 import { FaHeart } from 'react-icons/fa';
 import { Button } from '../ui/button';
+import FavoriteToggleForm from './FavoriteToggleForm';
 
 type FavoriteToggleProps = {
   propertyId: string;
 };
 
-const FavoriteToggleForm = ({ propertyId }: FavoriteToggleProps) => {
-  return (
-    <Button size="icon" variant="outline" className="p-2 cursor pointer">
-      <FaHeart />
-    </Button>
-  );
+const FavoriteToggleButton = async ({ propertyId }: FavoriteToggleProps) => {
+  const { userId } = auth();
+  const favoriteId = await fetchFavoriteId({ propertyId });
+
+  if (!userId) {
+    return <CardSignInButton />;
+  }
+
+  return <FavoriteToggleForm propertyId={propertyId} favoriteId={favoriteId} />;
 };
 
-export default FavoriteToggleForm;
+export default FavoriteToggleButton;
--- components/card/FavoriteToggleForm.tsx
+++ components/card/FavoriteToggleForm.tsx
@@ -1,5 +1,28 @@
-const FavoriteToggleForm = () => {
-  return <div>FavoriteToggleForm</div>;
+'use client';
+
+import { toggleFavoriteAction } from '@/utils/actions';
+import { usePathname } from 'next/navigation';
+import { CardSubmitButton } from '../form/Buttons';
+import FormContainer from '../form/FormContainer';
+
+type FavoriteToggleFormProps = {
+  propertyId: string;
+  favoriteId: string | null;
 };
 
+function FavoriteToggleForm({ propertyId, favoriteId }: FavoriteToggleFormProps) {
+  const pathname = usePathname();
+
+  const toggleAction = toggleFavoriteAction.bind(null, {
+    propertyId,
+    favoriteId,
+    pathname,
+  });
+
+  return (
+    <FormContainer action={toggleAction}>
+      <CardSubmitButton isFavorite={favoriteId ? true : false} />
+    </FormContainer>
+  );
+}
 export default FavoriteToggleForm;
