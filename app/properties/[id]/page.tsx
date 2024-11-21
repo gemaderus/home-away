b6633377ad7cc28b4f913a0c2db5ed83137d256c
gemaderus <hi@gemaderus.com>
2024-11-21 09:10:00 +0100

    feat: add logic for reviewDoesNotExist

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -8,9 +8,12 @@ import ImageContainer from '@/components/properties/ImageContainer';
 import PropertyDetails from '@/components/properties/PropertyDetails';
 import ShareButton from '@/components/properties/SharedButton';
 import UserInfo from '@/components/properties/UserInfo';
+import PropertyReviews from '@/components/reviews/PropertyReviews';
+import SubmitReview from '@/components/reviews/SubmitReview';
 import { Separator } from '@/components/ui/separator';
 import { Skeleton } from '@/components/ui/skeleton';
-import { fetchPropertiesDetail } from '@/utils/actions';
+import { fetchPropertiesDetail, findExistingReview } from '@/utils/actions';
+import { auth } from '@clerk/nextjs/server';
 import dynamic from 'next/dynamic';
 import { redirect } from 'next/navigation';
 
@@ -21,10 +24,14 @@ const DynamicMap = dynamic(() => import('@/components/properties/PropertyMap'), 
 
 const PropertyDetailPage = async ({ params }: { params: { id: string } }) => {
   const property = await fetchPropertiesDetail(params.id);
+  if (!property) redirect('/');
   const firstName = property?.profile.firstName as string;
   const profileImage = property?.profile.profileImage as string;
+  const { userId } = auth();
+  const isNotOwner = property.profile.clerkId !== userId;
+  const reviewDoesNotExist =
+    userId && isNotOwner && !(await findExistingReview(userId, property.id));
 
-  if (!property) redirect('/');
   const { baths, bedrooms, beds, guests } = property;
   const details = { baths, bedrooms, beds, guests };
 
@@ -56,6 +63,9 @@ const PropertyDetailPage = async ({ params }: { params: { id: string } }) => {
           <BookingCalendar />
         </div>
       </section>
+      {reviewDoesNotExist && <SubmitReview propertyId={property.id} />}
+
+      <PropertyReviews propertyId={property.id} />
     </section>
   );
 };
