8f2c531cab69a25ac48f7c72430307d6ab8c4b7b
gemaderus <hi@gemaderus.com>
2024-11-21 09:09:31 +0100

    feat: add actions for reviews

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -82,3 +82,9 @@ export const propertySchema = z.object({
   }),
   amenities: z.string(),
 });
+
+export const createReviewSchema = z.object({
+  propertyId: z.string(),
+  rating: z.coerce.number().int().min(1, { message: 'rating must be between 1 and 5' }).max(5),
+  comment: z.string().min(10, { message: 'comment must be at least 10 characters' }).max(1000),
+});
