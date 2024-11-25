0fe5d123b8f732c10e94d2cdfeaf4821a572c489
gemaderus <hi@gemaderus.com>
2024-11-25 07:05:06 +0100

    feat: add components for admin page

---
+++ components/admin/ChartsContainer.tsx
@@ -0,0 +1,9 @@
+const ChartsContainer = () => {
+  return (
+    <div>
+      <h1>Charts</h1>
+    </div>
+  );
+};
+
+export default ChartsContainer;
---
+++ components/admin/StatsCard.tsx
@@ -0,0 +1,19 @@
+import { Card, CardHeader } from '../ui/card';
+
+type StatsCardProps = {
+  title: string;
+  value: number;
+};
+
+const StatsCard = ({ title, value }: StatsCardProps) => {
+  return (
+    <Card className="bg-muted">
+      <CardHeader className="flex justify-between flex-row items-center">
+        <h3 className="text-3xl font-bold capitalize">{title}</h3>
+        <span className="text-primary text-5xl font-extrabold">{value}</span>
+      </CardHeader>
+    </Card>
+  );
+};
+
+export default StatsCard;
