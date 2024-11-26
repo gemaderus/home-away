c62355d02ef5a440cd58720aa745774e3ccff3af
gemaderus <hi@gemaderus.com>
2024-11-26 13:06:19 +0100

    feat: refactor format date

--- utils/format.ts
+++ utils/format.ts
@@ -2,7 +2,7 @@ export const formatCurrency = (amount: number | null) => {
   const value = amount || 0;
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
-    currency: 'USD',
+    currency: 'EUR',
     minimumFractionDigits: 0,
     maximumFractionDigits: 0,
   }).format(value);
@@ -12,10 +12,15 @@ export function formatQuantity(quantity: number, noun: string): string {
   return quantity === 1 ? `${quantity} ${noun}` : `${quantity} ${noun}s`;
 }
 
-export const formatDate = (date: Date | string) => {
-  return new Intl.DateTimeFormat('us-US', {
+export const formatDate = (date: Date | string, onlyMonth?: boolean) => {
+  const options: Intl.DateTimeFormatOptions = {
     year: 'numeric',
     month: 'long',
-    day: 'numeric',
-  }).format(new Date(date));
+  };
+
+  if (!onlyMonth) {
+    options.day = 'numeric';
+  }
+
+  return new Intl.DateTimeFormat('us-US', options).format(new Date(date));
 };
