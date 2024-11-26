97d3e5b30be25e88b1ea46aeb65e03552ef96e4d
gemaderus <hi@gemaderus.com>
2024-11-26 13:05:30 +0100

    feat: add Stats in reservations

--- app/reservations/page.tsx
+++ app/reservations/page.tsx
@@ -3,6 +3,8 @@ import EmptyList from '@/components/home/EmptyList';
 import { fetchReservations } from '@/utils/actions';
 import Link from 'next/link';
 
+import Stats from '@/components/reservations/Stats';
+
 import {
   Table,
   TableBody,
@@ -22,50 +24,53 @@ const ReservationsPage = async () => {
   }
 
   return (
-    <div className="mt-16">
-      <h4 className="mb-4 capitalize">total reservations: {reservations.length}</h4>
-      <Table>
-        <TableCaption>A list of recent revervations</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Property Name</TableHead>
-            <TableHead>Country</TableHead>
-            <TableHead>Nights</TableHead>
-            <TableHead>Total</TableHead>
-            <TableHead>Check In</TableHead>
-            <TableHead>Check Out</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {reservations.map((reservation) => {
-            const { id, orderTotal, totalNights, checkIn, checkOut } = reservation;
-            const { id: propertyId, name, country } = reservation.property;
-            const startDate = formatDate(checkIn);
-            const endDate = formatDate(checkOut);
+    <>
+      <Stats />
+      <div className="mt-16">
+        <h4 className="mb-4 capitalize">total reservations: {reservations.length}</h4>
+        <Table>
+          <TableCaption>A list of recent revervations</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Property Name</TableHead>
+              <TableHead>Country</TableHead>
+              <TableHead>Nights</TableHead>
+              <TableHead>Total</TableHead>
+              <TableHead>Check In</TableHead>
+              <TableHead>Check Out</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {reservations.map((reservation) => {
+              const { id, orderTotal, totalNights, checkIn, checkOut } = reservation;
+              const { id: propertyId, name, country } = reservation.property;
+              const startDate = formatDate(checkIn);
+              const endDate = formatDate(checkOut);
 
-            return (
-              <TableRow key={id}>
-                <TableCell>
-                  <Link
-                    href={`/properties/${propertyId}`}
-                    className="underline text-muted-foreground tracking-wide"
-                  >
-                    {name}
-                  </Link>
-                </TableCell>
-                <TableCell>
-                  <CountryFlagAndName countryCode={country} />
-                </TableCell>
-                <TableCell>{totalNights}</TableCell>
-                <TableCell>{formatCurrency(orderTotal)}</TableCell>
-                <TableCell>{startDate}</TableCell>
-                <TableCell>{endDate}</TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </div>
+              return (
+                <TableRow key={id}>
+                  <TableCell>
+                    <Link
+                      href={`/properties/${propertyId}`}
+                      className="underline text-muted-foreground tracking-wide"
+                    >
+                      {name}
+                    </Link>
+                  </TableCell>
+                  <TableCell>
+                    <CountryFlagAndName countryCode={country} />
+                  </TableCell>
+                  <TableCell>{totalNights}</TableCell>
+                  <TableCell>{formatCurrency(orderTotal)}</TableCell>
+                  <TableCell>{startDate}</TableCell>
+                  <TableCell>{endDate}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </div>
+    </>
   );
 };
 
