ff3aaeee3b28b4889a2e16af9312e3f5270473cd
gemaderus <hi@gemaderus.com>
2024-11-26 13:04:02 +0100

    feat: add integration con stripe in checkout

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,5 +1,30 @@
+'use client';
+import { EmbeddedCheckout, EmbeddedCheckoutProvider } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import axios from 'axios';
+import { useSearchParams } from 'next/navigation';
+import React, { useCallback } from 'react';
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string);
+
 const CheckoutPage = () => {
-  return <h1 className="text-3xl">CheckoutPage</h1>;
+  const searchParams = useSearchParams();
+  const bookingId = searchParams.get('bookingId');
+
+  const fetchClientSecret = useCallback(async () => {
+    const response = await axios.post('/api/payment', { bookingId: bookingId });
+    return response.data.clientSecret;
+  }, []);
+
+  const options = { fetchClientSecret };
+
+  return (
+    <div id="checkout">
+      <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+        <EmbeddedCheckout />
+      </EmbeddedCheckoutProvider>
+    </div>
+  );
 };
 
 export default CheckoutPage;
